84db6ca707e0d4f3929a7b72517198f519549a74
Michel Nickbon <michel.lemponen@gmail.com>
2020-09-20 17:24:39 +0200

    Added IStockInfoServices.cs

--- Main.cs
+++ Main.cs
@@ -2,15 +2,22 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using StockMarketMonitor.Services;
 
 namespace StockMarketMonitor
 {
     public static class StockMarketMonitor
     {
         [FunctionName("StockMarketMonitor")]
-        public async static Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public async static Task Run([TimerTrigger("0 */5 * * * *", RunOnStartup = true)]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            // Get stock info
+            var stockService = new StockInfoService();
+            stockService.GetStockInfo();
+
+            // Send email notification
         }
     }
 }
--- Services/StockInfoService.cs
+++ Services/StockInfoService.cs
@@ -1,7 +1,30 @@
-﻿namespace StockMarketMonitor.Services
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace StockMarketMonitor.Services
 {
 	class StockInfoService
 	{
+        private HttpWebRequest webRequest;
+        private HttpWebResponse webResponse;
+        private Stream stream;
+        private readonly string url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo";
 
-	}
+		public void GetStockInfo()
+		{
+            webRequest = (HttpWebRequest)WebRequest.Create(string.Format(url));
+            webRequest.Method = "GET";
+            webResponse = (HttpWebResponse)webRequest.GetResponse();
+
+            var responseString = string.Empty;
+            using (stream = webResponse.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8);
+                responseString = reader.ReadToEnd();
+            }
+        }
+    }
 }
