28ffabcfa42e91f5e0208f1a351683334c8e1ba2
Michel Nickbon <michel.lemponen@gmail.com>
2020-09-30 16:10:00 +0200

    Added unit test and test project

--- StockMarketMonitor.UnitTest/StockMarketMonitor.UnitTest/ServicesTests.cs
+++ StockMarketMonitor.UnitTest/StockMarketMonitor.UnitTest/ServicesTests.cs
@@ -1,6 +1,8 @@
 using Moq;
+using Newtonsoft.Json.Linq;
 using StockMarketMonitor.Services;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace StockMarketMonitor.UnitTest
@@ -9,22 +11,45 @@ namespace StockMarketMonitor.UnitTest
 	{
 		private Mock<INotificationService> _notificationService;
 		private Mock<IHttpRequestService> _httpRequestService;
-
-		private readonly string testKey = "&apikey=GDUJE493";
-		private readonly string testBaseUrl = "www.testing.com/query?function=TIME_SERIES_DAILY&";
-		private readonly string testSymbol = "IBM";
+		private readonly string symbol = "IBM";
 
 		public ServicesTests()
 		{
+			Environment.SetEnvironmentVariable("API_KEY", "&apikey=TESTEST");
+			Environment.SetEnvironmentVariable("API_STOCK_BASE_URL", "https://www.testing.com/query?function=TEST_SERIES&");
 			_httpRequestService = new Mock<IHttpRequestService>();
-			_httpRequestService.Setup(h => h.BuildEndpointURL(testSymbol)).Returns(string.Concat(testBaseUrl, "symbol=", testSymbol, testKey));
 		}
 
 		[Fact]
 		public void HttpRequestService_BuildsEndpointURL()
 		{
-			var expected = "www.testing.com/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=GDUJE493";
-			Assert.Equal(expected, _httpRequestService.Object.BuildEndpointURL(testSymbol));
+			var url = new HttpRequestService().BuildEndpointURL(symbol);
+			Assert.Equal("https://www.testing.com/query?function=TEST_SERIES&symbol=IBM&apikey=TESTEST", url);
 		}
+
+		[Fact]
+		public async void HttpRequestService_ReturnsStockQuotes()
+		{
+			_httpRequestService.Setup(h => h.GetStockQuotes(It.IsAny<string>())).ReturnsAsync(new JObject());
+			var response = await _httpRequestService.Object.GetStockQuotes("www.test.com");
+			Assert.True(response.GetType() == typeof(JObject));
+		}
+
+		[Fact]
+		public void NotificationService_EmailMessageBodyGetBuilt()
+		{
+			var stocks = new List<Stock>() { new Stock("Symbol") { OpenPrice = 0 } };
+			var messageBody = new NotificationService().BuildEmailMessageBody(stocks);
+
+			Assert.NotNull(messageBody);
+			Assert.Contains("Markets are open, here are you opening quotes", messageBody);
+		}
+
+		[Fact]
+		public void NotificationService_EmailGetsSent()
+		{
+			throw new NotImplementedException();
+		}
+
 	}
 }
